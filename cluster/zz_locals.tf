2c72dfd11f3e7a9c333c95c905b06d7cfcf45f40
Simon Weald <glitchcrab-github@simonweald.com>
2023-03-04 16:58:24 +0000

    convert host list to use hagroups for VM placement

--- cluster/zz_locals.tf
+++ cluster/zz_locals.tf
@@ -1,11 +1,27 @@
 locals {
-  master_count       = 0
+  hastate            = "enabled"
+  master_count       = 1
+  master_description = "kubernetes management cluster master"
   name_stub          = "room101-a7d-mc"
   vmid_base          = 400
   vmid_offset_master = 10
   worker_count       = 0
+  worker_description = "kubernetes managment cluster worker"
 
-  host_list = ["host-01", "host-02", "host-03"]
+  host_list = [
+    {
+      name    = "host-01"
+      hagroup = "ha-group-1"
+    },
+    {
+      name    = "host-02"
+      hagroup = "ha-group-2"
+    },
+    {
+      name    = "host-03"
+      hagroup = "ha-group-3"
+    }
+  ]
 
   pm_host_address = data.vault_generic_secret.terraform_generic.data["host"]
 }
