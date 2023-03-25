c732af0292f24522b173b8cc50e89a212db82d09
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2023-03-25 20:21:16 +0000

    configure oncreate and create instance serials (#8)
    * bump module version and ensure nodes do not power on after creation
    
    * generate a random string to be used as the instance's serial

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,10 +7,16 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Generate a random string to be used for the VM's serial.
+
 ### Changed
 
 - General tidy in preparation for cluster creation.
 - Enable HA masters (3).
+- Bump `glitchcrab/terraform-module-proxmox-instance` to 1.6.0
+- Do not power on nodes after creation.
 
 ## [0.1.0] - 2022-12-19
 
--- cluster/resources.tf
+++ cluster/resources.tf
@@ -2,13 +2,21 @@ resource "macaddress" "master_net0_mac" {
   count = local.master_count
 }
 
+resource "random_string" "master_serial" {
+  count = local.master_count
+
+  length  = 16
+  special = false
+  upper   = false
+}
+
 module "master_instances" {
   count = local.master_count
 
-  source = "github.com/glitchcrab/terraform-module-proxmox-instance?ref=v1.5.0"
+  source = "github.com/glitchcrab/terraform-module-proxmox-instance?ref=v1.6.0"
 
   pve_instance_name        = "master${count.index}-${local.name_stub}"
-  pve_instance_description = local.master_description
+  pve_instance_description = "${local.master_description} (serial: ${random_string.master_serial[count.index].result})"
   vmid                     = local.vmid_base + count.index + local.vmid_offset
 
   target_node   = lookup(local.host_list[count.index], "name")
@@ -23,6 +31,8 @@ module "master_instances" {
   sockets = var.resource_cpu_sockets
   memory  = var.resource_memory
   boot    = var.boot
+
+  oncreate = local.master_oncreate
 
   network_interfaces = [{
     bridge  = var.net0_network_bridge
@@ -42,13 +52,21 @@ resource "macaddress" "worker_net0_mac" {
   count = local.worker_count
 }
 
+resource "random_string" "worker_serial" {
+  count = local.worker_count
+
+  length  = 16
+  special = false
+  upper   = false
+}
+
 module "worker_instances" {
   count = local.worker_count
 
-  source = "github.com/glitchcrab/terraform-module-proxmox-instance?ref=v1.5.0"
+  source = "github.com/glitchcrab/terraform-module-proxmox-instance?ref=v1.6.0"
 
   pve_instance_name        = "worker${count.index}-${local.name_stub}"
-  pve_instance_description = local.worker_description
+  pve_instance_description = "${local.worker_description} (serial: ${random_string.worker_serial[count.index].result})"
   vmid                     = local.vmid_base + count.index + local.vmid_offset + 5
 
   target_node   = lookup(local.host_list[count.index], "name")
@@ -63,6 +81,8 @@ module "worker_instances" {
   sockets = var.resource_cpu_sockets
   memory  = var.resource_memory
   boot    = var.boot
+
+  oncreate = local.worker_oncreate
 
   network_interfaces = [{
     bridge  = var.net0_network_bridge
