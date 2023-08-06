a45ebd90a3c8dd0e89a954e46df688722b28624c
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2023-08-06 18:03:51 +0200

    remove serials from instances (#12)

--- cluster/zz_versions.tf
+++ cluster/zz_versions.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "Telmate/proxmox"
       version = "~> 2.9.14"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4.3"
-    }
     time = {
       source  = "hashicorp/time"
       version = "0.9.1"
